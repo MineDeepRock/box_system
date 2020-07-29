a5306003954b76b245562427a651f19a3279253c
suinua <maatinosippo@gmail.com>
2020-07-29 21:39:45 +0900

    add fromString method

--- src/box_system/models/Box.php
+++ src/box_system/models/Box.php
@@ -9,4 +9,20 @@ abstract class Box
     const NAME = "";
     const SECOND_LIMIT = 0;
     const RANGE = 0;
+
+    static function fromString(string $text): ?Box {
+        switch ($text) {
+            case AmmoBox::NAME:
+                return new AmmoBox();
+                break;
+            case FlareBox::NAME:
+                return new FlareBox();
+                break;
+            case MedicineBox::NAME:
+                return new MedicineBox();
+                break;
+        }
+
+        return null;
+    }
 }
