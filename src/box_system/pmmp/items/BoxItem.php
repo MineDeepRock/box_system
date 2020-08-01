bca9d4e15315a4a37575440d0a9ce1826543cf18
suinua <maatinosippo@gmail.com>
2020-08-01 16:48:39 +0900

    fromBox

--- src/box_system/pmmp/items/BoxItem.php
+++ src/box_system/pmmp/items/BoxItem.php
@@ -4,9 +4,27 @@
 namespace box_system\pmmp\items;
 
 
+use box_system\models\AmmoBox;
+use box_system\models\Box;
+use box_system\models\FlareBox;
+use box_system\models\MedicineBox;
 use pocketmine\item\Item;
 
 abstract class BoxItem extends Item
 {
+    static function fromBox(Box $box): ?BoxItem {
+        switch ($box::NAME) {
+            case AmmoBox::NAME:
+                return new SpawnAmmoBoxItem();
+                break;
+            case FlareBox::NAME:
+                return new SpawnFlareBoxItem();
+                break;
+            case MedicineBox::NAME:
+                return new SpawnMedicineBoxItem();
+                break;
+        }
 
+        return null;
+    }
 }
